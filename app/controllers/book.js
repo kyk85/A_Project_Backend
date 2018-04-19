89051061b467dcd77083bf9b263b1ae482d716b8
KYK <kay2kay1@gmail.com>
2018-04-19 23:46:32 +0800

    0.4

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -15,7 +15,7 @@ exports.createBook = function(req, res, next){
         author: req.body.author,
         isbn: req.body.isbn,
         coverArt: req.body.coverArt,
-        // owner: req.params.user_id
+        owner: req.body.userId
     }, function(err, book){
         if(err){
             res.send(err)
