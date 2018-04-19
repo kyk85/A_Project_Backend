5d22ef80f4ac5bf8cae9b6b48a9c575a6018db12
KYK <kay2kay1@gmail.com>
2018-04-20 05:06:19 +0800

    0.4.5.5a - Item Editing

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -68,14 +68,13 @@ exports.editBook = function(req, res, next){
     // var book = req.params.book_id
     // var test = User.findById(req.params.user_id)
     // console.log(test)
-    User.find({library:{_id:req.params.book_id}},
-        {$set: {
-            title:req.body.title,
-            author:req.body.author,
-            isbn:req.body.isbn,
-            coverArt:req.body.coverArt
-        }}).then((data)=>{
-        // res.json(data)
+    User.findByIdAndUpdate(req.params.user_id,
+        {'library._id':req.params.book_id},
+        {$set:{
+            'library.$.title':req.body.title
+        }}
+        ).then((data)=>{
+        console.log(data)
         User.markModified('library');
         User.save();
     });
