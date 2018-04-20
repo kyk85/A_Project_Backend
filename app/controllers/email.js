d1e78dc22735deb8ca84f03a9e95e1fec4796285
KYK <kay2kay1@gmail.com>
2018-04-21 00:08:23 +0800

    Inquiry function added

--- app/controllers/email.js
+++ app/controllers/email.js
@@ -2,11 +2,11 @@ var nodemailer = require('nodemailer');
 // var Inquiry = require('../models/inquiry')
 
 exports.inquiryReply = function(req, res) {
-    var inquiry = {
-        contactName: contactName,
-        contactEmail: contactEmail,
-        contactInquiry: contactInquiry
-    }
+    // var inquiry = {
+    //     contactName: contactName,
+    //     contactEmail: contactEmail,
+    //     contactInquiry: contactInquiry
+    // }
     var transporter = nodemailer.createTransport({
         host: process.env.EHOST,
         port: process.env.EPORT,
@@ -19,10 +19,10 @@ exports.inquiryReply = function(req, res) {
     
     let mailOptions = {
         from: '"YK" <kay2kay1@gmail.com>',
-        to: this.inquiry.contactEmail,
-        subject: 'Thank you for your inquiry ' + this.inquiry.contactName + ' !',
+        to: req.body.contactEmail,
+        subject: 'Thank you for your inquiry ' + req.body.contactName + ' !',
         text: 'Hello',
-        html: 'Your inquiry:<br>' + this.inquiry.contactInquiry
+        html: 'Your inquiry:<br>' + req.body.contactInquiry
     };
 
     // res.json({message:"asd"})
