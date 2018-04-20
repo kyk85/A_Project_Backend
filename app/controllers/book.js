0e126e4034c21146fdc56c607f8b6bf43364342e
YK <kay2kay1@gmail.com>
2018-04-20 13:46:42 +0800

    Fixed Edit Item

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -68,23 +68,28 @@ exports.editBook = function(req, res, next){
     // var book = req.params.book_id
     // var test = User.findById(req.params.user_id)
     // console.log(test)
-    User.findByIdAndUpdate(req.params.user_id,
-        {'library._id':req.params.book_id},
-        {$set:{
-            'library.$.title':req.body.title
-        }}
-        ).then((data)=>{
-        console.log(data)
-        User.markModified('library');
-        User.save();
-    });
+    User.findOneAndUpdate(
+        // {$unwind:'$library'},
+        // {$match: {'library.id': 'ObjectId(' + req.params.book_id + ')'}},
+        // {$group: {_id:"$_id", library: {$push:'$library'}}},
+        // {'_id' : req.params.user_id},
+        // {'library':{$elemMatch:{ '_id' : req.params.book_id}}},
+        {'_id':req.params.user_id,'library._id': req.params.book_id},
+        {'library.$.title': req.body.title,
+        'library.$.author': req.body.author,
+        'library.$.isbn': req.body.isbn,
+        'library.$.coverArt': req.body.coverArt})
+        .then(()=>{
+            User.markModified('library');
+            User.save();
+        })
 
-    User.findById(req.params.user_id, 'library', function(err, data){
-        if(err){
-            res.send(err);
-        }
-        res.json(data);
-    })
+        User.findById(req.params.user_id, 'library', function(err, data){
+            if(err){
+                res.send(err);
+            }
+            res.json(data);
+        })
 }
 
 exports.deleteBook = function(req, res, next){
